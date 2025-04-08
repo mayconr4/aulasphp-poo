d2349bb889d6c6418b7ed443eef485af9c7145d7
mayconr4 <mayconhs2007@gmail.com>
2025-04-08 11:17:57 -0300

    Cria objeto e testa estrutura

--- index.php
+++ index.php
@@ -8,6 +8,18 @@
 <body>
     <h1>PHP com POO - Exemplo 01</h1>
     <hr>    
-    
+<?php 
+// Carregando/importando a classe
+require_once "src/Cliente.php"; 
+
+// Criando objeto/ instancia da classe
+$clienteA  = new Cliente();
+$clienteB  = new Cliente();
+  
+?>
+
+<h2>Testes de acesso aos recursos da classe atráves do objeto</h2> 
+
+<pre><?=var_dump($clienteA, $clienteB)?></pre>
 </body>
 </html>
