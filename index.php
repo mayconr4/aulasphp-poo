9c83a9e5c56f1839bd3c8cbb77bc6e4d5061a4cb
mayconr4 <mayconhs2007@gmail.com>
2025-04-10 09:21:50 -0300

    poo

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
-<!DOCTYPE html>
+<<!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo - 01</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>orientação a objeto</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    
 </head>
-<body>
-    <h1>PHP com POO - Exemplo 01</h1>
+<body>  
+    <div class="container">
+        <h1 class=" text-center">exemplo1 php poo</h1>
     <hr>    
 <?php 
 // Carregando/importando a classe
