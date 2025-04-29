a33cb7090c1bafa3fd6f75735e7f6c11bacfdec1
mayconr4 <mayconhs2007@gmail.com>
2025-04-29 09:59:30 -0300

    adicionado composer json

--- src/Models/Cliente.php
+++ src/Models/Cliente.php
@@ -1,65 +1,69 @@
 <?php
+
 namespace MeuProjeto\Models;
 /*Usamos o use para indicar a utilização de otras classes\enums que temos */
+
 use MeuProjeto\Enums\Situacao;
 
 // Usamos o 'use' para indicar a utilização de classesn internas 
 use InvalidArgumentException;
-abstract class Cliente 
+
+abstract class Cliente
 {
     private string $nome;
-    private string $email; 
+    private string $email;
     private Situacao $situacao;
 
-    
-    public function __construct(string $nome, string $email, Situacao $situacao = Situacao::INATIVO) 
+
+    public function __construct(string $nome, string $email, Situacao $situacao = Situacao::INATIVO)
     {
         $this->setNome($nome);
-        $this->setEmail($email);  
-        $this->setSituacao($situacao); 
-               
-    }   
+        $this->setEmail($email);
+        $this->setSituacao($situacao);
+    }
 
-    public function relatorio():string { 
+    public function relatorio(): string
+    {
         return "<div> 
             <h4> {$this->getNome()} </h4> 
             <p><b>Contato:</b> {$this->getEmail()}</p>
-        </div>";  
+        </div>";
     }
 
     /*Visisbilidade protegida: desta forma, este método pode ser usado aqui em Cliente (Superclasse) e també nas subclasses  o acesso externo (no nosso caso em index) continua sendo bloqueado.**/
-    protected function setSituacao(Situacao $situacao):void{ 
+    protected function setSituacao(Situacao $situacao): void
+    {
         $this->situacao = $situacao;
-    } 
-
-    public function getSituacao():Situacao { 
-       return $this->situacao ;
     }
 
-    private function setNome(string $nome): void 
+    public function getSituacao(): Situacao
+    {
+        return $this->situacao;
+    }
+
+    private function setNome(string $nome): void
     {
         $this->nome = $nome;
     }
 
 
-    private function setEmail(string $email): void 
+    private function setEmail(string $email): void
     {
-        if( !filter_var($email, FILTER_VALIDATE_EMAIL) ){
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             throw new InvalidArgumentException("E-mail inválido!");
         }
 
         $this->email = $email;
     }
 
-    public function getNome(): string 
+    public function getNome(): string
     {
         return $this->nome;
     }
 
 
-    public function getEmail(): string 
+    public function getEmail(): string
     {
         return $this->email;
     }
-    
 }
