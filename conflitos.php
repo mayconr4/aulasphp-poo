cdac2980a603ac6add398a91099e6ab81253f3e3
mayconr4 <mayconhs2007@gmail.com>
2025-04-29 09:23:20 -0300

    teste de conflitos de classes

---
+++ conflitos.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conflito de classes</title>
+</head>
+<body>
+    <h1>Evitando conflito de classes</h1>
+
+<?php
+
+use Fornecedores\Pagamento;
+use Prestadores\Pagamento as PrestadoresPagamento; // Neste caso, com apelido
+
+require_once "src/Fornecedores/Pagamento.php";
+require_once "src/Prestadores/Pagamento.php";
+
+// Criando objeto para usar as classes de pagamento
+$pagamentoFornecedor = new Pagamento();
+$pagamentoPrestador = new PrestadoresPagamento();
+
+?>
+    
+</body>
+</html>
